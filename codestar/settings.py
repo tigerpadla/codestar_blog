e0a7a023df3501b8296c2bf6e0154be96def423f
tigerpadla <xythryll@gmail.com>
2025-11-12 13:58:28 +0000

    Add post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,6 +32,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
+    "127.0.0.1",
 ]
 
 
@@ -88,6 +89,8 @@ WSGI_APPLICATION = "codestar.wsgi.application"
 # }
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+CSRF_TRUSTED_ORIGINS = ["https://*.codeinstitute-ide.net/", "https://*.herokuapp.com"]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
