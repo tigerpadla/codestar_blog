116a5a7de4f01700ea9ea06ac0663dd16e0d5f27
tigerpadla <xythryll@gmail.com>
2025-11-13 21:55:11 +0000

    Create the main page using Django's generic views

--- codestar/urls.py
+++ codestar/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from blog.views import my_blog
+from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', my_blog, name='blog'),
-    path('admin/', admin.site.urls),
+    path("", include("blog.urls"), name="blog-urls"),
+    path("admin/", admin.site.urls),
 ]
